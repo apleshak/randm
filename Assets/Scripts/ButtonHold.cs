e72e270940c8b8011b1a77c78c06b70db493be80
apleshakov <pleshakov.anton@gmail.com>
2017-05-08 00:50:04 -0400

    Fixed buttin hold

--- Assets/Scripts/ButtonHold.cs
+++ Assets/Scripts/ButtonHold.cs
@@ -10,11 +10,12 @@ public class ButtonHold : MyMonoBehaviour
 	public string moveAxis = "x";
 	Vector3 initialPos;
 	Vector3 initialScale;
-	public Coroutine currentCoroutine;
+	Coroutine currentCoroutine;
 	public GameObject actionTarget;
 	public GameObject pushableElement;
-	public float buttonPushDist = 0.5f;
-	public float buttonPushSpeed = 0.1f;
+    Vector3 pushableElementInitPos;
+	float buttonPushDist = 0.5f;
+	float buttonPushSpeed = 0.1f;
 	public float pushSpeed = 0.1f;
 	public float pushDist = 1.0f;
 	public float shrinkSpeed = 0.1f;
@@ -28,40 +29,45 @@ public class ButtonHold : MyMonoBehaviour
 	{
 		initialPos = actionTarget.transform.position;
 		initialScale = actionTarget.transform.localScale;
+        pushableElementInitPos = pushableElement.transform.position;
 		pushed = false;
 	}
 
 	public void OnTriggerStay2D (Collider2D other)
 	{
-		if ((other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy") && !active && !pushed)
+		if ((other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy") && !pushed)
 		{
 			if (currentCoroutine != null)
 				StopCoroutine(currentCoroutine);
-			active = true;
 			actingObj = other.gameObject;
+            active = true;
 			currentCoroutine = StartCoroutine(PerformAction());
 		}
-	}
+
+        active = true;
+    }
 	
 	public void OnTriggerExit2D (Collider2D other)
 	{
-		if (active && other.gameObject == actingObj)
+		if (other.gameObject == actingObj)
 		{
 			StopCoroutine(currentCoroutine);
-			active = false;
+            pushed = false;
 			actingObj = null;
-			currentCoroutine = StartCoroutine(ReverseAction());
-		}
-	}
+            active = false;
+            Debug.Log("Stopping button");
+            currentCoroutine = StartCoroutine(ReverseAction());
+        }
+    }
 	
 	IEnumerator PerformAction()
 	{
 		//Button pushing logic
 		Transform t = pushableElement.transform;
-        Vector3 target = t.position + new Vector3(0, -buttonPushDist, 0);
+        Vector3 target = pushableElementInitPos + new Vector3(0, -buttonPushDist, 0);
         float accum = 0.0f;
 
-        while (t != null && t.position != target)
+        while (active && t != null && t.position != target)
         {
             yield return new WaitForEndOfFrame();
             accum += buttonPushSpeed;
@@ -73,36 +79,54 @@ public class ButtonHold : MyMonoBehaviour
 
         //Start moving and/or shrinking target object
 		t = actionTarget.transform;
-		Vector3 targetPos;
-		Vector3 targetScale;
+		Vector3 targetPos = t.position;
+		Vector3 targetScale = t.localScale;
 		
-		if (moveAxis == "x")
-		{
-			targetPos = new Vector3(initialPos.x-pushDist, initialPos.y, initialPos.z);
-		}
-		else
-		{
-			targetPos = new Vector3(initialPos.x, initialPos.y-pushDist, initialPos.z);
-		}
+        if (move)
+        {
+            if (moveAxis == "x")
+            {
+                targetPos = new Vector3(initialPos.x - pushDist, initialPos.y, initialPos.z);
+            }
+            else
+            {
+                targetPos = new Vector3(initialPos.x, initialPos.y - pushDist, initialPos.z);
+            }
+        }
 
-		targetScale = new Vector3(initialScale.x, initialScale.y-shrinkAmount, initialScale.z);
-		
-		while (Vector3.Distance(t.position, targetPos) > pushSpeed || (Vector3.Distance(t.localScale, targetScale)) < 0)
-		{
-			Debug.Log(Vector3.Distance(t.position, targetPos));
-			if (move)
-				t.position = new Vector3(t.position.x-pushSpeed, t.position.y, t.position.z);
+        if (shrink)
+		    targetScale = new Vector3(initialScale.x, initialScale.y-shrinkAmount, initialScale.z);
+
+        while (Vector3.Angle(t.position, targetPos) > 0 || (Vector3.Angle(t.localScale, targetScale)) > 0)
+        {
+            if (move)
+            {
+                if (moveAxis == "x")
+                {
+                    t.position = new Vector3(t.position.x - pushSpeed, t.position.y, t.position.z);
+                }
+                else
+                {
+                    t.position = new Vector3(t.position.x, t.position.y - pushSpeed, t.position.z);
+                }
+            }
 			if (shrink)
 				t.localScale = new Vector3(t.localScale.x, t.localScale.y-shrinkSpeed, t.localScale.z);
 			yield return new WaitForEndOfFrame();
 		}
+
+        //Sit and wait for someone to get off the button
+        while (pushed)
+        {
+            yield return new WaitForEndOfFrame();
+        }
 	}
 	
 	IEnumerator ReverseAction()
 	{
 		//Button logic
 		Transform t = pushableElement.transform;
-		Vector3 target = t.position + new Vector3(0, buttonPushDist, 0);
+        Vector3 target = pushableElementInitPos;
         float accum = 0.0f;
 
         while (t != null && t.position != target)
@@ -116,13 +140,22 @@ public class ButtonHold : MyMonoBehaviour
         pushed = false;
         active = false;
 
-		t = actionTarget.transform;
+        t = actionTarget.transform;
 
-		while (Vector3.Distance(t.position, initialPos) > pushSpeed || (Vector3.Distance(t.localScale, initialScale) > 0)) //add shrink
+		while (Vector3.Angle(t.position, initialPos) > 0 || (Vector3.Angle(t.localScale, initialScale) > 0)) //add shrink
 		{
-			if (move)
-				t.position = new Vector3(t.position.x+pushSpeed, t.position.y, t.position.z);
-			if (shrink)
+            if (move)
+            {
+                if (moveAxis == "x")
+                {
+                    t.position = new Vector3(t.position.x + pushSpeed, t.position.y, t.position.z);
+                }
+                else
+                {
+                    t.position = new Vector3(t.position.x, t.position.y + pushSpeed, t.position.z);
+                }
+            }
+            if (shrink)
 				t.localScale = new Vector3(t.localScale.x, t.localScale.y+shrinkSpeed, t.localScale.z);
 			yield return new WaitForEndOfFrame();
 		}
